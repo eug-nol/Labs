4609aa5656842073f58fe88e6ca39181d593a8c9
euge <futureyuu133@gmail.com>
2024-03-19 11:06:55 -0300

    complete unit 1 lab 1

--- Unidad.1.Capitulo.1.Lab.1.Introduccion/Lab1/Lab1/Program.cs
+++ Unidad.1.Capitulo.1.Lab.1.Introduccion/Lab1/Lab1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("¡Hola mundo!");
+        }
+    }
+}
