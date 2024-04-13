ad4e72db098f5e051579a576c5156992aef25412
euge <futureyuu133@gmail.com>
2024-04-13 17:40:01 -0300

    complete unit 1.2 lab 1

---
+++ Unidad.1.Capitulo.2.Lab.1.IDE/Lab1/Consola/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///System.Console.WriteALine("Esta línea va a dar error :p");
+            System.Console.WriteLine("Esta línea es correcta");
+        }
+    }
+}
